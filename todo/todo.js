48482e70c1a3a94dd85dc5bd1b9ea9fc87725900
nh894864 <nicholas.hustak@broadcom.com>
2022-04-29 17:37:25 -0400

    Moved todo list into a directive

--- todo/todo.js
+++ todo/todo.js
@@ -1,28 +1,43 @@
-angular.module('todoApp', [])
-  .controller('TodoListController', function() {
+angular
+  .module("todo", [])
+  .directive("todolist", function () {
+    "use strict";
+
+    return {
+      restrict: "AE",
+      replace: true,
+      scope: {},
+      templateUrl: "./todo/todo.view.html",
+    };
+  })
+  .controller("TodoListController", function () {
     var todoList = this;
     todoList.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
- 
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+      { text: "learn AngularJS", done: true },
+      { text: "build an AngularJS app", done: false },
+    ];
+
+    todoList.addTodo = function () {
+      todoList.todos.push({ text: todoList.todoText, done: false });
+      todoList.todoText = "";
     };
- 
-    todoList.remaining = function() {
+
+    todoList.remaining = function () {
       var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
+      angular.forEach(todoList.todos, function (todo) {
         count += todo.done ? 0 : 1;
       });
       return count;
     };
- 
-    todoList.archive = function() {
+
+    todoList.archive = function () {
       var oldTodos = todoList.todos;
       todoList.todos = [];
-      angular.forEach(oldTodos, function(todo) {
+      angular.forEach(oldTodos, function (todo) {
         if (!todo.done) todoList.todos.push(todo);
       });
     };
   });
+
+
+  angular.module('app', ['todo'])
